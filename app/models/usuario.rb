2370d187225aa21939c040c455ac59ea7903170e
Nelson Jerez <nelson.jerezv@gmail.com>
2016-03-07 12:06:28 -0300

    update controller useuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,7 +1,6 @@
 class Usuario < ActiveRecord::Base
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
-	validates :NOMBRE_USUARIO, presence: true
 	has_many :Vendedor, :foreign_key => "ID_USUARIO" 
 	has_many :Bodeguero, :foreign_key => "ID_USUARIO" 
 	devise :database_authenticatable, :registerable,
